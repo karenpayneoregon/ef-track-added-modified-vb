e4ef1f7c51009e851e815b9f81414193fdbf7649
Karen Payne <paynekaren@comcast.net>
2020-03-05 06:32:49 -0800

    Updated before changes actions

--- Backend/Context.vb
+++ Backend/Context.vb
@@ -14,67 +14,19 @@ Partial Public Class Context
     Public Overridable Property Contact1 As DbSet(Of Contact1)
 
     Protected Overrides Sub OnModelCreating(ByVal Builder As DbModelBuilder)
-
-
     End Sub
     ''' <summary>
-    ''' Responsible for setting created and last updated information
-    ''' prior to saving changes.
+    ''' Responsible for setting created, last updated
+    ''' and soft delete property before SaveChanges or SaveChangesAsync
     ''' </summary>
-    Private Sub SetTimestamps()
+    Private Sub BeforeSave()
 
-        Dim entities = ChangeTracker.Entries().
-                Where(
-                    Function(entityItem)
-                        Return TypeOf entityItem.Entity Is BaseEntity AndAlso
-                               (entityItem.State = EntityState.Added OrElse
-                                entityItem.State = EntityState.Modified)
-                    End Function)
-
-        Console.WriteLine(entities.Count())
-        Dim currentUsername = Environment.UserName
-
-        For Each currentEntry As DbEntityEntry In entities
-
-            If currentEntry.State = EntityState.Added Then
-                CType(currentEntry.Entity, BaseEntity).CreatedAt = Date.Now
-                CType(currentEntry.Entity, BaseEntity).CreatedBy = currentUsername
-            End If
-
-            CType(currentEntry.Entity, BaseEntity).LastUpdated = Date.Now
-            CType(currentEntry.Entity, BaseEntity).LastUser = currentUsername
-
-        Next
-    End Sub
-    ''' <summary>
-    ''' For any entity that is marked for deletion set it's
-    ''' state to modified and set the IsDelete property to true
-    ''' </summary>
-    Private Sub PrepareDeleted()
-
-        For Each EntryItem In ChangeTracker.Entries()
-            If EntryItem.State = EntityState.Deleted Then
-                EntryItem.State = EntityState.Modified
-                'CType(EntryItem.Entity, BaseEntity).IsDeleted = True
-            End If
-        Next
-
-    End Sub
-    Public Overrides Function SaveChangesAsync() As Task(Of Integer)
-        ChangeTracker.DetectChanges()
-        SetTimestamps()
-        PrepareDeleted()
-
-        Return MyBase.SaveChangesAsync()
-
-    End Function
-
-    Public Overrides Function SaveChanges() As Integer
         ChangeTracker.DetectChanges()
 
         For Each currentEntry As DbEntityEntry In ChangeTracker.Entries()
 
             If currentEntry.State = EntityState.Added OrElse currentEntry.State = EntityState.Modified Then
+
                 currentEntry.Property("LastUpdated").CurrentValue = Date.Now
                 currentEntry.Property("LastUser").CurrentValue = Environment.UserName
 
@@ -82,17 +34,45 @@ Partial Public Class Context
                     currentEntry.Property("CreatedAt").CurrentValue = Date.Now
                     currentEntry.Property("CreatedBy").CurrentValue = Environment.UserName
                 End If
+
             ElseIf currentEntry.State = EntityState.Deleted Then
+
+                currentEntry.Property("LastUpdated").CurrentValue = Date.Now
+                currentEntry.Property("LastUser").CurrentValue = Environment.UserName
+
                 ' Change state to modified and set delete flag
                 currentEntry.State = EntityState.Modified
                 CType(currentEntry.Entity, BaseEntity).IsDeleted = True
+
             End If
 
         Next
 
+    End Sub
+
+    Public Overrides Function SaveChangesAsync() As Task(Of Integer)
+
+        BeforeSave()
+        Return MyBase.SaveChangesAsync()
+
+    End Function
+
+    Public Overrides Function SaveChanges() As Integer
+
+        BeforeSave()
         Return MyBase.SaveChanges()
 
     End Function
+    ''' <summary>
+    ''' Simple method to filter out IsDeleted records 
+    ''' </summary>
+    ''' <typeparam name="TEntity"></typeparam>
+    ''' <param name="query"></param>
+    ''' <returns></returns>
+    ''' <remarks>
+    ''' This can be done in a generic method also,
+    ''' keeping things simple here for starters.
+    ''' </remarks>
     Public Function ApplyActiveFilter(Of TEntity As Contact1)(query As IQueryable(Of TEntity)) As IQueryable(Of TEntity)
         Return query.Where(Function(entity) entity.IsDeleted.Value = False)
     End Function
--- ContactsForm/Form1.vb
+++ ContactsForm/Form1.vb
@@ -1,6 +1,9 @@
 ﻿Imports System.Data.Entity
 Imports Backend
 
+''' <summary>
+''' For testing only, this form will eventually be removed.
+''' </summary>
 Public Class Form1
     Private dbContext As New Context
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -14,9 +17,25 @@ Public Class Form1
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim contact1 = dbContext.Contact1.FirstOrDefault(Function(contact) contact.ContactId = 1)
+        Dim contact2 = dbContext.Contact1.FirstOrDefault(Function(contact) contact.ContactId = 2)
 
         dbContext.Entry(contact1).State = EntityState.Deleted
 
-        dbContext.SaveChanges()
+        contact2.FirstName = "Mary"
+
+        Dim affected = dbContext.SaveChanges()
+        Console.WriteLine(affected)
+    End Sub
+
+    Private Async Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim contact1 = dbContext.Contact1.FirstOrDefault(Function(contact) contact.ContactId = 1)
+        Dim contact2 = dbContext.Contact1.FirstOrDefault(Function(contact) contact.ContactId = 2)
+
+        dbContext.Entry(contact1).State = EntityState.Deleted
+
+        contact2.FirstName = "Anne"
+
+        Dim affected = Await dbContext.SaveChangesAsync()
+        Console.WriteLine(affected)
     End Sub
 End Class
